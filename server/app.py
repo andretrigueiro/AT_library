16d36247c97738d50622f683bd5c9e709b26bdd1
andretrigueiro <andretrigueiro.kf@gmail.com>
2022-01-17 02:37:04 -0300

    add PUT request

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database.db import BOOKS
 import json
+import uuid
 
 # configuration
 DEBUG = True
@@ -44,6 +45,14 @@ def validate_post(requested):
 def search_book_on_list(value):
     return next((item for item in BOOKS if item['title'] == value), None)
 
+# remove book if id is found
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
 # ----------------------------------------------------------------------------------------
 
 # sanity check route
@@ -53,7 +62,7 @@ def ping_pong():
 
 # ----------------------------------------------------------------------------------------
 
-# route handler of books
+# route handler of books - GET AND POST
 @app.route('/books', methods=['GET', 'POST'])
 def all_books():
     response_object = {'status': 'success'}
@@ -66,6 +75,7 @@ def all_books():
                 response_object['message'] = 'Book already added!'
             else:
                 BOOKS.append({
+                    'id': uuid.uuid4().hex,
                     'title': post_data.get('title'),
                     'author': post_data.get('author'),
                     'read': post_data.get('read')
@@ -76,6 +86,28 @@ def all_books():
             response_object['message'] = 'Invalid request!'
     else:
         response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+# route handler of books - PUT
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        if validate_post(post_data):
+            if remove_book(book_id):
+                BOOKS.append({
+                    'id': uuid.uuid4().hex,
+                    'title': post_data.get('title'),
+                    'author': post_data.get('author'),
+                    'read': post_data.get('read')
+                })
+                response_object['message'] = 'Book updated!'
+            else:
+                response_object['message'] = 'Book not founded!'
+        else:
+            response_object = {'status': 'error'}
+            response_object['message'] = 'Invalid request!'
     return jsonify(response_object)
 
 # ----------------------------------------------------------------------------------------
